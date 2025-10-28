a268e42d43c71b7d6936b9fc935562a23e2200a1
NindyaShafira24 <nindyashafira585@gmail.com>
2025-10-28 17:45:32 +0700

    Inisialisasi project dan menambahkan file main.dart

---
+++ bin/main.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+  print('=== Program Cek Bilangan Prima & Faktorial ===\n');
+
+  stdout.write('Masukkan bilangan bulat positif: ');
+  int n = int.parse(stdin.readLineSync()!);
+
+  // Percabangan untuk validasi input
+  if (n <= 0) {
+    print('Input tidak valid! Masukkan angka lebih dari 0.');
+  } else {
+    // Perulangan untuk mengecek bilangan prima
+    bool prima = true;
+    if (n == 1) {
+      prima = false;
+    } else {
+      for (int i = 2; i <= n ~/ 2; i++) {
+        if (n % i == 0) {
+          prima = false;
+          break;
+        }
+      }
+    }
+
+    print('\n=== Hasil Pengecekan ===');
+    if (prima) {
+      print('$n adalah bilangan prima');
+    } else {
+      print('$n bukan bilangan prima');
+    }
+
+    // Perulangan untuk menghitung faktorial
+    int faktorial = 1;
+    for (int i = 1; i <= n; i++) {
+      faktorial *= i;
+    }
+
+    print('Nilai faktorial dari $n adalah: $faktorial');
+  }
+
+  print('\n=== Program Selesai ===');
+}
